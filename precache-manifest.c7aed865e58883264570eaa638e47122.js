aa62a365db1caffd5b466726494b199d9db16112
Sagnick Kundu <sagnickkundu97@gmail.com>
2022-04-11 17:38:16 +0530

    Updates

---
+++ precache-manifest.c7aed865e58883264570eaa638e47122.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a904c30d6a0765f0e7b4ebaa0d5f66",
+    "url": "/mern-stack-client/index.html"
+  },
+  {
+    "revision": "907add27d43f2bef0868",
+    "url": "/mern-stack-client/static/css/main.5e486d42.chunk.css"
+  },
+  {
+    "revision": "49637bf630646d5d78c9",
+    "url": "/mern-stack-client/static/js/2.207c5b4e.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/mern-stack-client/static/js/2.207c5b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "907add27d43f2bef0868",
+    "url": "/mern-stack-client/static/js/main.b8041756.chunk.js"
+  },
+  {
+    "revision": "c62587a70b1841857931",
+    "url": "/mern-stack-client/static/js/runtime-main.a5b3310a.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/mern-stack-client/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/mern-stack-client/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
