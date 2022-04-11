9eadd1769278e1723a322e26041aebb5f1012dc6
Sagnick Kundu <sagnickkundu97@gmail.com>
2022-04-11 15:09:03 +0530

    testing

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -17,9 +17,9 @@ export const getPost = (id) => async (dispatch) => {
   export const getPosts = (page) => async (dispatch) => {
     try {
       dispatch({ type: START_LOADING });
-      
-      const { data: { data, currentPage, numberOfPages } } = await api.fetchPosts(page);
       console.log('i am here');
+      const { data: { data, currentPage, numberOfPages } } = await api.fetchPosts(page);
+      
       dispatch({ type: FETCH_ALL, payload: { data, currentPage, numberOfPages } });
       dispatch({ type: END_LOADING });
     } catch (error) {
